32adeffaeb306724f908a1df1b8d39f4c7298049
melunyann <melune25@gmail.com>
2026-09-09 16:29:07 +0900

    Update vite.config.js

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -1,7 +1,1 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+import { defineConfig } from 'vite' import vue from '@vitejs/plugin-vue' export default defineConfig({ base: '/cm-list/', plugins: [vue()], })
